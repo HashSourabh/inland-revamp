1c7785dec7fac04d67bf4ce2190499c342139109
hashsourabh <hashsourabh@gmail.com>
2026-03-12 17:37:19 +0530

    worked on update logo

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -11,7 +11,7 @@ export default function MainLayout({ children }: MainLayoutProps) {
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <main className="flex-grow pt-[172px] xs:pt-[145px] sm:pt-[120px]">{children}</main>
+      <main className="flex-grow pt-[195px] xs:pt-[167px] sm:pt-[139px] xl:pt-[150px]">{children}</main>
       <Footer />
     </div>
   );
